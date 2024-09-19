94daeff5d121cb518abf8ae48f996e51cebe4516
solomontudu <tukintudu@gmail.com>
2024-09-19 20:24:01 +0530

    added the dynamicaly add equations to the DOM function, and also added the spacer to the equation list in the beggining and the end aswell.

--- script.js
+++ script.js
@@ -32,6 +32,12 @@ const wrongFormat = [];
 
 // Scroll
 
+// display game page
+function showGamePage() {
+  gamePage.hidden = false;
+  countdownPage.hidden = true;
+}
+
 // get random number up to a max number
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
@@ -68,29 +74,45 @@ function createEquations() {
     equationsArray.push(equationObject);
   }
   shuffle(equationsArray);
-  console.log("equations array:", equationsArray);
+}
+
+// add equations to DOM
+function equationsToDOM() {
+  equationsArray.forEach((equation) => {
+    // item
+    const item = document.createElement("div");
+    item.classList.add("item");
+    // equation text
+    const equationText = document.createElement("h1");
+    equationText.textContent = equation.value;
+    // append
+    item.appendChild(equationText);
+    itemContainer.appendChild(item);
+  });
 }
 
 // Dynamically adding correct/incorrect equations
-// function populateGamePage() {
-//   // Reset DOM, Set Blank Space Above
-//   itemContainer.textContent = '';
-//   // Spacer
-//   const topSpacer = document.createElement('div');
-//   topSpacer.classList.add('height-240');
-//   // Selected Item
-//   const selectedItem = document.createElement('div');
-//   selectedItem.classList.add('selected-item');
-//   // Append
-//   itemContainer.append(topSpacer, selectedItem);
+function populateGamePage() {
+  // Reset DOM, Set Blank Space Above
+  itemContainer.textContent = "";
+  // Spacer
+  const topSpacer = document.createElement("div");
+  topSpacer.classList.add("height-240");
+  // Selected Item
+  const selectedItem = document.createElement("div");
+  selectedItem.classList.add("selected-item");
+  // Append
+  itemContainer.append(topSpacer, selectedItem);
 
-//   // Create Equations, Build Elements in DOM
+  // Create Equations, Build Elements in DOM
+  createEquations();
+  equationsToDOM();
 
-//   // Set Blank Space Below
-//   const bottomSpacer = document.createElement('div');
-//   bottomSpacer.classList.add('height-500');
-//   itemContainer.appendChild(bottomSpacer);
-// }
+  // Set Blank Space Below
+  const bottomSpacer = document.createElement("div");
+  bottomSpacer.classList.add("height-500");
+  itemContainer.appendChild(bottomSpacer);
+}
 
 // get the value from selected radio button
 function getRadioValue() {
@@ -124,7 +146,8 @@ function showCountdown() {
   splashPage.hidden = true;
   countdownPage.hidden = false;
   countdownStart();
-  createEquations();
+  populateGamePage();
+  setTimeout(showGamePage, 400);
 }
 
 // form that decides amount of questons
